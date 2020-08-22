1853933437ff1111ca14d4ef879a525e1403bc6c
Yun Cheolhun <oliverne@gmail.com>
2020-08-22 15:11:58 +0900

    Add more use cases for Model

--- src/models/Todo.model.ts
+++ src/models/Todo.model.ts
@@ -1,25 +1,30 @@
 import Backbone from 'backbone';
 
-interface ITodo {
+export interface ITodoModel {
   title: string;
   completed?: boolean;
   createdAt?: number;
 }
 
-export default class Todo extends Backbone.Model<ITodo, {}> {
-  initialize() {
+export default class TodoModel extends Backbone.Model<ITodoModel, {}> {
+  constructor(attrs: ITodoModel, options?: Backbone.ModelSetOptions) {
+    super(attrs, options);
     console.log('TodoModel initialized');
+    this.on('change', this.onChange);
+
     this.set('createdAt', Date.now());
     this.set('completed', false);
   }
 
-  /*
-  constructor(attrs: ITodo, options?: Backbone.ModelSetOptions) {
-    super(attrs, options);
-  }
-  */
-
   toggle() {
     this.set('completed', !this.get('completed'));
+  }
+
+  get completed() {
+    return this.get('completed');
+  }
+
+  onChange() {
+    console.log('TodoModel changed', this.changed);
   }
 }
